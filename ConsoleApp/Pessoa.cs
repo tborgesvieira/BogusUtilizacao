06dc8de59671bdb7a8d00c304139848a591a1359
Thiago Borges Vieira <thiago@upgradesolutions.com.br>
2019-04-28 09:35:11 -0300

    Exemplo Bogus
    Código de exemplo do bogus.

---
+++ ConsoleApp/Pessoa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class Pessoa
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using Bogus;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pessoa = new Faker<Pessoa>("pt_BR")
+                .RuleFor(c => c.Id, f => f.Random.Guid())
+                .RuleFor(c => c.Nome, f => f.Name.FullName(Bogus.DataSets.Name.Gender.Female))
+                .RuleFor(c => c.DataNascimento, f => f.Date.Past(15))
+                .Generate();
+
+            Console.WriteLine(@"Dados da pessoa");
+            Console.WriteLine($"ID: {pessoa.Id}");
+            Console.WriteLine($"Nome: {pessoa.Nome}");
+            Console.WriteLine($"Data de Nacimento: {pessoa.DataNascimento.ToShortDateString()}");
+            Console.WriteLine("------------------");
+
+
+            Console.WriteLine("");
+            Console.WriteLine("Exemplo lista:");
+
+            var pessoas = new Faker<Pessoa>("pt_BR")
+                .RuleFor(c => c.Id, f => f.Random.Guid())
+                .RuleFor(c => c.Nome, f => f.Name.FullName())
+                .RuleFor(c => c.DataNascimento, f => f.Date.Past(15))
+                .Generate(3);
+
+            foreach (var pessoaFake in pessoas)
+            {
+                Console.WriteLine(@"Dados da pessoa");
+                Console.WriteLine($"ID: {pessoaFake.Id}");
+                Console.WriteLine($"Nome: {pessoaFake.Nome}");
+                Console.WriteLine($"Data de Nacimento: {pessoaFake.DataNascimento.ToShortDateString()}");
+                Console.WriteLine("------------------");
+            }
+
+        }
+    }
+}
